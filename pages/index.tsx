30a4dc35dbce34b8547d86ba25951eb9eed66ef2
Alex Ramirez <alex@mis.fans>
2021-10-29 00:30:21 -0500

    New features table

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className="overflow-hidden h-screen">
+    <div>
       <HeroSection/>
       <FeaturesTable/>
     </div>
